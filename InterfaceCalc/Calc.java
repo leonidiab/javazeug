bc4f2d8ed6c050d00745fdcea0a41553a5c92f53
leonidia <leonidia_barreto@yahoo.com.br>
2019-04-30 23:24:24 -0300

    atualizacao

--- InterfaceCalc/Calc.java
+++ InterfaceCalc/Calc.java
@@ -1,8 +1,8 @@
 //precisa por as exceções aqui
 //criou a propria classe de erro
 public interface Calc{
-	public int soma(int a, int v) throw ErrParEx;
-	public int sub(int a, int b);
+	public int soma(int a, int b) throw ErrParEx;
+	public int sub(int a, int b) throw ErrParEx;
 	public double mult(double a, double b);
-	public double div(double a, double b);
+	public double div(double a, double b) throw ErrParEx;
 }
